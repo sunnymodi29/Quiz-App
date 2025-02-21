19223ebaa50caf57621b84c568da5e957c44d283
Sunny Modi <modisunny25@gmail.com>
2025-02-21 20:47:47 +0530

    Quiz App Features

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,43 @@
+import { useState, useCallback, act } from "react";
+import QUESTIONS from "../questions";
+import { Question } from "./Question";
+import Summary from "./Summary";
+
+const Quiz = () => {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+};
+
+export default Quiz;
